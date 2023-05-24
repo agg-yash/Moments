50760701c3e089c8b77cae713ab9d1e390bc92c6
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-05-24 18:52:59 +0530

    add styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,23 @@ import moments from "./images/moments.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Moments
         </Typography>
-        <img src={moments} alt="moments" height="60" />
+        <img
+          className={classes.image}
+          src={moments}
+          alt="moments"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
