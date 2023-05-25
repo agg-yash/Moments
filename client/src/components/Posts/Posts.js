bf99c63bb28534e59cb2a32e7f6da1f2875629c0
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-05-25 16:53:41 +0530

    post components and making post beautiful with material-ui

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { CircularProgress, Grid } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
@@ -7,13 +8,16 @@ import useStyles from "./styles";
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
-  console.log(posts);
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid className="container" container alignItems="stretch" spacing={3}>
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
