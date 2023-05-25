3b7b5652b70ec1b331f5315c1ec566c33c36c793
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-05-25 16:56:59 +0530

    add css for svg background

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+import "./index.css";
 
 import reducers from "./reducers";
 
