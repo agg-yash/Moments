994a3fd0432a579b1e7b3c7f784b4c2721b9faf5
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-05-27 14:34:01 +0530

    add pagination component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
@@ -9,10 +9,11 @@ import Auth from "./components/Auth/Auth";
 const App = () => {
   return (
     <BrowserRouter>
-      <Container maxwidth="lg">
+      <Container maxwidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/" exact element={<Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
           <Route path="/auth" exact element={<Auth />} />
         </Routes>
       </Container>
