27a9e33b89fc8b0f0e66f131a60803a75774ee0f
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-05-27 14:54:05 +0530

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <BrowserRouter>
       <Container maxwidth="xl">
@@ -14,7 +17,13 @@ const App = () => {
         <Routes>
           <Route path="/" exact element={<Navigate to="/posts" />} />
           <Route path="/posts" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route
+            path="/auth"
+            exact
+            element={!user ? <Auth /> : <Navigate to="/posts" />}
+          />
         </Routes>
       </Container>
     </BrowserRouter>
